4d1cc1623b044ff6d85addb0c46d82bcdfd0e896
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-08-01 11:41:20 +0200

    PositionReport component added to chair and shelf.

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -8,7 +8,6 @@ UPositionReport::UPositionReport()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
 	// ...
 }
 
@@ -17,8 +16,7 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty!"));
 	
 }
 
