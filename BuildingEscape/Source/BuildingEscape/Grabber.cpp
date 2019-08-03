2989b6306e7f0940e9c8ed024168fdd8a52f6bfb
Nemanja Kuzmić <kuzmicnemanja@live.com>
2019-08-03 23:12:44 +0200

    Added UE_LOG at Pawn_BP so it reports every frame location and rotation.

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -1,7 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Grabber.h"
+#include "Engine/World.h"
 #include "GameFramework/Actor.h"
+#include "GameFramework/PlayerController.h"
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -28,6 +31,13 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get player viewpoint this tick
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
+	// TODO Log out to test
+	UE_LOG(LogTemp, Warning, TEXT("Player Location: %s,Rotation: %s"),*PlayerViewPointLocation.ToString(),*PlayerViewPointRotation.ToString());
+	// Ray-cast out to reach distance
+	// See what we hit
 }
 
